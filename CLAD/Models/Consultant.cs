9e7d10809c9bc80dc26c18f7f1a4c95d7b424b58
bo <bowelter@hotmail.com>
2019-04-09 09:56:24 +0200

    modelmooimaken

--- CLAD/Models/Consultant.cs
+++ CLAD/Models/Consultant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace CLAD.Models
 {
     public class Consultant
     {
+        [Display(Name = "Omschrijving")]
         public string Description { get; set; }
+
+        [Display(Name = "Naam")]
         public string DisplayName { get; set; }
+
     }
 }
