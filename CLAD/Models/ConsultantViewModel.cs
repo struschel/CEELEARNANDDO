9e788c8bcca5d3bd4aac5cde9269bc9a9474df9b
bo <bowelter@hotmail.com>
2019-04-18 14:56:04 +0200

    begin consultantpagina

---
+++ CLAD/Models/ConsultantViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CLAD.Models
+{
+    public class ConsultantViewModel
+    {
+        public List<Article> Articles { get; set; }
+        public User User { get; set; }
+
+    }
+}
